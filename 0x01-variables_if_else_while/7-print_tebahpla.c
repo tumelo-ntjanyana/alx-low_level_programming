6b80f6567f3a65807c2d05f95e332f21bb85c62a
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-12 03:38:15 -0700

    7-print_tebahpla.c project

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet in lowercase letters in reverse
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(letter);
+	}
+		putchar('\n');
+		return (0);
+}
