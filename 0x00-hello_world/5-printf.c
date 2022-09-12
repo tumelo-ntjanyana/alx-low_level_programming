dc9147c7a30bca321ae74b3caa4284773b8c34c7
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-11 23:16:05 -0700

    5-printf.c project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,17 +3,12 @@
  *
  *main - Entry point
  *
- * 
+ *
  *Return: Always 0 (Success)
  *
  */
-
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
