417a2c409a04b872c28255c9f3c8db57fb1c1679
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-12 04:19:32 -0700

    100-print_comb3.c project

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -41,18 +41,20 @@ int main(void)
 	while (d < 10)
 	{
 		c = 0;
-		while (c < 10)	
-		{																if (d != c && d < c)	
-			{										
-				putchar('0' + d);	
-				putchar('0' + c);								
+		while (c < 10)
+		{
+			if (d != c && d < c)	
+			{							
+				putchar('0' + d);
+				putchar('0' + c);
 		if (c + d != 17)
-		{								
+		{						
 			putchar(',');
-			putchar(' ');																																}
+			putchar(' ');
 		}
-		c++;																	}
-		d++;		
+		}
+		c++;															}
+		d++;
 	}
 		putchar('\n');
 		return (0);
