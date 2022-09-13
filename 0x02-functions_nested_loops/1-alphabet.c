4ae6cd703eb456f38e5c6c22a6feafa71c77ebc4
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-13 06:15:33 -0700

    1-alphabet.c project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet->prints the alphabet, in lowercase, followed by a new line
  * Return: Always 0
  * function that prints the alphabet, in lowercase, followed by a new line
  */
