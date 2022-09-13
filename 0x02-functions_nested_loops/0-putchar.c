683a7930ee6021a755d25576ec49935cece53255
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-13 00:48:31 -0700

    0-putchar.c project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char tumelo[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(tumelo[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
