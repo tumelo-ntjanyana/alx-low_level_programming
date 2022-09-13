2d7daa9dc17ddfb655cb88b3d2f75a4252103972
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-13 06:19:18 -0700

    2-print_alphabet_x10.c project

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * function that prints 10 times the alphabet, in lowercase, followed by a new line
+ * print_alphabet-> function that prints 10 times the alphabet, in lowercase, followed by a new line
  * Return - 0
  */
 void print_alphabet_x10(void)
