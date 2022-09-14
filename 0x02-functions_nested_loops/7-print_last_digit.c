b1b13423babefdc4db53484104f298b08d17b18c
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-14 01:51:44 -0700

    7-print_last_digit.c project

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - a function that prints the last digit of a number
- * @nld: number's last digit result
+ * @n: number's last digit result
  * Return: value of the last digit
  */
 
