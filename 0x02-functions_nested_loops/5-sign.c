9cd168229a5320bae7cfafccd26b14e23e3c6619
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-14 01:17:00 -0700

    5-sign.c project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,33 +10,19 @@ int print_sign()
 {
 	int n;
 
-	n = print_sign(98);
-
-	_putchar(',');
-	_putchar(' ');
-	_putchar(n + '0');
-	_putchar('\n');
-
-	n = print_sign(0);
-
-	_putchar(',');
-	_putchar(' ');
-	_putchar(n + '0');
-	_putchar('\n');
-
-	n = print_sign(0xff);
-
-	_putchar(',');
-	_putchar(' ');
-	_putchar(n + '0');
-	_putchar('\n');
-
-	n = print_sign(-1);
-
-	_putchar(',');
-	_putchar(' ');
-	_putchar(n + '0');
-	_putchar('\n');
-
-	return (0);
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
 }
