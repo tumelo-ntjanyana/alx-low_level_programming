d8349b0f2a6ce0436ca4bf4440f35ce95cf28bd4
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-14 03:00:50 -0700

    100-times_table.c project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,11 @@
 
 /**
  * print_times_table - Entry point
- * @a: input
+ * @n: input
  * Return: Always 0 (Success)
  */
 
-void print_times_table(int a)
+void print_times_table(int n)
 {
 	return (0);
 }
