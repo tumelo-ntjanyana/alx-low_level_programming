b6e562b68b24a2be078fe0d8e04e32414d2ecffb
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-14 04:18:04 -0700

    103-fibonacci.c project

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,18 +15,15 @@ int main(void)
 	{
 		fibsum = fib1 + fib2;
 		if (fibsum > 4000000)
-		{
+
 			break;
 			if ((fibsum % 2) == 0)
-			{
+
 				tot_sum += fibsum;
 
 				fib1 = fib2;
 				fib2 = fibsum;
 
-			}
 			printf("%.0f\n", tot_sum);
 			return (0);
-		}
-	}
 }
