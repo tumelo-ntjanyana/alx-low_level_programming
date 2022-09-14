f324cd65090459c8dc055894d2373c35d1036a77
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-14 03:09:03 -0700

    100-times_table.c project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,10 +3,40 @@
 /**
  * print_times_table - Entry point
  * @n: input
- * Return: Always 0 (Success)
+ * Return: Number matrix
  */
 
 void print_times_table(int n)
 {
-	return (0);
+	int a;
+	int b;
+	int c;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (c > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
