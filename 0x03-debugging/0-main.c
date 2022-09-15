53068968f05a31ef56f77be7a07c7b0b99986d73
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-15 00:54:47 -0700

    0-main.c project

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	positive_or_negative(a);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(void);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
