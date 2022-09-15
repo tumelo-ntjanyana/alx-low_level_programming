81ea11e643db80080b41f490dfff761b7e2957fd
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-15 01:11:15 -0700

    1-main.c project

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	printf("Infinite loop incoming :(\n");
+
+	a = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+
