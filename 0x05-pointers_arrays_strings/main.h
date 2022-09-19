5f9b7579f9464940aecec46bc6e6695d58cfec23
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-19 01:53:52 -0700

    0-reset_to_98.c project

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+int _putchar(char c);                                                                                                                   
+int main(void);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-/* MAIN_H */
+#endif
