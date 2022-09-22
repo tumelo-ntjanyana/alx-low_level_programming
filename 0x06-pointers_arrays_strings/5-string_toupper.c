065639bb10f2b49bbb094699c0878d240668bcdd
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-22 04:16:21 -0700

    5-string_toupper.c project

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letter
+ * @n: string to be changed
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
