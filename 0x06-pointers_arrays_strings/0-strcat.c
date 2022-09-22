dba7d88f8b3f0a405c710839eb17f1ca4f18aa8a
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-22 02:55:02 -0700

    0-strcat.c project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-	for (ondex = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
