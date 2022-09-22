29b225eeff3f5e73402587c6c62c65a0f7106fc8
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-22 03:44:53 -0700

    2-strncpy.c project

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _strcmp - Compares two strings but add inputted numbers of bytes
- * @dest: string to be appended upon
- * @src: string to be completed at end of dest
- * @n: integer parameter to compare index to
+ * _strncpy -  function that copies a string..
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The copy limit
  * Return: void
  */
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	i = 0;
-
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
@@ -24,7 +23,6 @@ char *_strncpy(char *dest, char *src, int n);
 	{
 		dest[i] = '\0';
 		i++;
-
 	}
 
 	return (dest);
