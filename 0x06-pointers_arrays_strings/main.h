f29d76bbaf3a7d1c7c61aa422a1f2038b74fd1de
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-22 03:06:51 -0700

    0-strcat.c project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -9,7 +9,7 @@
  * char prototypes()
  */
 
-char *_strncat (char *dest, char *src, int n);
+char *_strncart(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
