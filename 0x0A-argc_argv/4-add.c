112a2441c08447be747559d4f5bfed1d6ac2b29c
Tumelo Ntjanyana <ntjanyanatumelo@gmail.com>
2022-09-30 05:26:09 -0700

    4-add.c project

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,7 @@
  * main - Prints the addition of positive numbers, followed by a new line.
  * @argc: The number of arguments passed to the program
  * @argv: An array of pointers to the arguments
- * Return: If one of the numbers contains symbols that are non-digits - 1. 
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
  * Otherwise - 0.
  */
 
